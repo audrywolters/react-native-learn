fa6578b71343ecc676180242d62e0667c9a1bf90
Audry Wolters <audrywolters@gmail.com>
2022-03-07 11:54:54 -0600

    added second attribute to friendList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,27 +4,36 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 const ListScreen = () => {
 
     const friendList = [
-        { name: 'Jill' },
-        { name: 'Megan' },
-        { name: 'Arik' },
-        { name: 'Mervyn' },
-        { name: 'Mina' },
-        { name: 'Purse' },
-        { name: 'Coat' },
-        { name: 'Gloves' }
+        { name: 'Jill',   age: 37 },
+        { name: 'Megan',  age: 37 },
+        { name: 'Arik',   age: 42 },
+        { name: 'Mervyn', age: 12 },
+        { name: 'Mina',   age: 16 },
+        { name: 'Purse',  age: 5 },
+        { name: 'Coat',   age: 6 },
+        { name: 'Gloves', age: 1 }
     ]
 
     return (
-        <FlatList 
+        <FlatList
+            showsVerticalScrollIndicator={ false }
             keyExtractor={ (f) => f.name }
             data={ friendList } 
             renderItem={ ({ item }) => {
-                    return <Text>{ item.name }</Text>
+                    return (
+                        <Text style={ styles.textStyle }>
+                            { item.name } - Age { item.age }
+                        </Text>
+                    )
                 }} 
         />
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 10
+    }
+});
 
 export default ListScreen;
