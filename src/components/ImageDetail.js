f5de33dd640318a6a34fb4317fd09c110bfdcac5
Audry Wolters <audrywolters@gmail.com>
2022-03-08 10:15:07 -0600

    passed ImageDetail image prop. Now displaying images dynamically.

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = ( props ) => {
 
     return (
-        <>
+        <View>
+            <Image source={ props.imageSource } />
             <Text>{ props.title }</Text>
-        </>
+        </View>
     )
 }
 
