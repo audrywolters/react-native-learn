96c20bdc04323807f9c0b3cbc528e75abaca69dd
Audry Wolters <audrywolters@gmail.com>
2022-03-08 13:58:01 -0600

    added Image Score prop and is displaying

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -8,17 +8,20 @@ const ImageScreen = () => {
         <View>
             <ImageDetail 
                 title='Forest' 
-                imageSource={ require('../../assets/forest.jpg') } 
+                imageSource={ require('../../assets/forest.jpg') }
+                score='9'
             />
 
             <ImageDetail 
                 title='Beach' 
-                imageSource={ require('../../assets/beach.jpg') } 
+                imageSource={ require('../../assets/beach.jpg') }
+                score='4'
             />
 
             <ImageDetail 
                 title='Mountain' 
-                imageSource={ require('../../assets/mountain.jpg') } 
+                imageSource={ require('../../assets/mountain.jpg') }
+                score='7'
             />
         </View>
     )
