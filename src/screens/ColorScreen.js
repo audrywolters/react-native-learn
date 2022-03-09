10f742bfe1b4557bb68abe6499856d43c9d452c7
Audry Wolters <audrywolters@gmail.com>
2022-03-09 17:46:15 -0600

    created ColorScreen and wired it to Home page.

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+
+const ColorScreen = () => {
+
+    return (
+        <View>
+            <Button title="Add a Color" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    myStyle: {
+        fontSize: 10
+    }
+});
+
+export default ColorScreen;
