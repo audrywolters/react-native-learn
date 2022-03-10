96437c4f6230f58f61ebb11d0dbc0cdfa61173d0
Audry Wolters <audrywolters@gmail.com>
2022-03-09 18:01:12 -0600

    added random colored view functionality

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React from 'react'
+import { View, StyleSheet, Button } from 'react-native'
 
 const ColorScreen = () => {
+	return (
+		<View>
+			<Button title="Add a Color" />
+			<View
+				style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+			/>
+		</View>
+	)
+}
 
-    return (
-        <View>
-            <Button title="Add a Color" />
-        </View>
-    )
+const randomRgb = () => {
+	const red = Math.floor(Math.random() * 256)
+	const green = Math.floor(Math.random() * 256)
+	const blue = Math.floor(Math.random() * 256)
+
+	return `rgb(${red}, ${green}, ${blue})`
 }
 
 const styles = StyleSheet.create({
-    myStyle: {
-        fontSize: 10
-    }
-});
+	myStyle: {
+		fontSize: 10
+	}
+})
 
-export default ColorScreen;
+export default ColorScreen
