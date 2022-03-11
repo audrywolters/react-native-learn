af498e10b7aefb5313c4bcaaeb9f31265ed63c9f
Audry Wolters <audrywolters@gmail.com>
2022-03-11 11:37:03 -0600

    renamed red -> redSum, blue/green etc. variables for clarity.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,23 +5,23 @@ import ColorCounter from '../components/ColorCounter'
 const COLOR_CREMENT = 15
 
 const SquareScreen = () => {
-	const [red, setRed] = useState(0)
-	const [green, setGreen] = useState(0)
-	const [blue, setBlue] = useState(0)
+	const [redSum, setRedSum] = useState(0)
+	const [greenSum, setGreenSum] = useState(0)
+	const [blueSum, setBlueSum] = useState(0)
 
 	const setColor = (color, change) => {
 		// color === 'red', 'green', 'blue'
-		// change === COLOR_CREMENT
+		// change === +/- COLOR_CREMENT
 
 		switch (color) {
 			case 'red':
-				red + change > 255 || red + change < 0 ? null : setRed(red + change)
+				redSum + change > 255 || redSum + change < 0 ? null : setRedSum(redSum + change)
 				return
 			case 'green':
-				green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+				greenSum + change > 255 || greenSum + change < 0 ? null : setGreenSum(greenSum + change)
 				return
 			case 'blue':
-				blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+				blueSum + change > 255 || blueSum + change < 0 ? null : setBlueSum(blueSum + change)
 				return
 			default:
 				return
@@ -49,7 +49,7 @@ const SquareScreen = () => {
 				style={{
 					height: 150,
 					width: 150,
-					backgroundColor: `rgb(${red}, ${green}, ${blue})`
+					backgroundColor: `rgb(${redSum}, ${greenSum}, ${blueSum})`
 				}}
 			/>
 		</View>
