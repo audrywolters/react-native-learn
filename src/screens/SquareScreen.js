675b79eb2001ed7dba6c3062d2009dbf06518f2f
Audry Wolters <audrywolters@gmail.com>
2022-03-11 10:01:57 -0600

    added onIncrease, onDecease callbacks to SquareScreen.Also added View that shows the color change. is working.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import ColorCounter from '../components/ColorCounter'
 
 const SquareScreen = () => {
+	const [red, setRed] = useState(0)
+	const [green, setGreen] = useState(0)
+	const [blue, setBlue] = useState(0)
+
+	console.log(`red: ${red}`)
+	console.log(`green: ${green}`)
+	console.log(`blue: ${blue}`)
+
 	return (
 		<View>
-			<ColorCounter color="Red" />
-			<ColorCounter color="Blue" />
-			<ColorCounter color="Green" />
+			<ColorCounter
+				onIncrease={() => setRed(red + 10)}
+				onDecrease={() => setRed(red - 10)}
+				color="Red"
+			/>
+			<ColorCounter
+				onIncrease={() => setGreen(green + 10)}
+				onDecrease={() => setGreen(green - 10)}
+				color="Green"
+			/>
+			<ColorCounter
+				onIncrease={() => setBlue(blue + 10)}
+				onDecrease={() => setBlue(blue - 10)}
+				color="Blue"
+			/>
+			<View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
 		</View>
 	)
 }
