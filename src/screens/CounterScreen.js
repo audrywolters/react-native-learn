04826c84c0549427394025ac63840a396cd399ef
Audry Wolters <audrywolters@gmail.com>
2022-03-12 10:05:57 -0600

    now useReducer instead of state. for reducer practice.

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,34 +1,49 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
+const COUNT_CREMENT = 1;
+
+const reducer = (state, action) => {
+	//  state === {count}: number
+	// action === {type}: 'increment' || 'decrement'
+	//			  {payload}: COUNT_CREMENT
+
+	switch (action.type) {
+		case 'increment':
+			// ADD the payload
+			return { ...state, count: state.count + action.payload }
+		case 'decrement':
+			// SUBTRACT	the payload
+			return { ...state, count: state.count - action.payload }
+		default:
+			return state
+	}
+}
+
 const CounterScreen = () => {
-	const [counter, setCounter] = useState(0)
+	const [state, dispatch] = useReducer(reducer, { count: 0 })
 
 	return (
 		<View>
 			<Button
 				title="Increase"
 				onPress={() => {
-					setCounter(counter + 1)
+					dispatch({ type: 'increment', payload: COUNT_CREMENT })
 				}}
 			/>
 
 			<Button
 				title="Decrease"
 				onPress={() => {
-					setCounter(counter - 1)
+					dispatch({ type: 'decrement', payload: COUNT_CREMENT })
 				}}
 			/>
 
-			<Text>Current Count: {counter}</Text>
+			<Text>Current Count: {state.count}</Text>
 		</View>
 	)
 }
 
-const styles = StyleSheet.create({
-	myStyle: {
-		fontSize: 10
-	}
-})
+const styles = StyleSheet.create({})
 
 export default CounterScreen
