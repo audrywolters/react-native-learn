d6713fa209bff26d819e289316afa893d93623a4
Audry Wolters <audrywolters@gmail.com>
2022-03-13 10:38:46 -0500

    working with FlexDirection, AlignItems, JustifyContent.

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,21 +4,25 @@ import { Text, View, StyleSheet } from 'react-native'
 const BoxScreen = () => {
 	return (
 		<View style={styles.viewStyle}>
-			<Text style={styles.textStyle}>BoxScreen</Text>
+			<Text style={styles.textStyle}>Child 1</Text>
+			<Text style={styles.textStyle}>Child 2</Text>
+			<Text style={styles.textStyle}>Child 3</Text>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	viewStyle: {
-		borderWidth: 3,
-		borderColor: 'black'
+		borderWidth: 5,
+		borderColor: 'green',
+		// flexDirection: 'row',
+		// alignItems: 'center',
+		justifyContent: 'space-between',
+		height: 200
 	},
 	textStyle: {
-		borderWidth: 10,
-		borderColor: 'pink',
-		margin: 20,
-		padding: 2
+		borderWidth: 3,
+		borderColor: 'pink'
 	}
 })
 
