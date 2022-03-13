49d4769213d66048758a419acb6f5a50d0895341
Audry Wolters <audrywolters@gmail.com>
2022-03-13 12:15:20 -0500

    expirementing with flexBox. want to save this for reference.

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import { Text, View, StyleSheet } from 'react-native'
 const BoxScreen = () => {
 	return (
 		<View style={styles.viewStyle}>
-			<Text style={styles.textStyle}>Child 1</Text>
-			<Text style={styles.textStyle}>Child 2</Text>
-			<Text style={styles.textStyle}>Child 3</Text>
+			<Text style={styles.textOneStyle}>Child 1</Text>
+			<Text style={styles.textTwoStyle}>Child 2</Text>
+			<Text style={styles.textThreeStyle}>Child 3</Text>
 		</View>
 	)
 }
@@ -15,15 +15,35 @@ const styles = StyleSheet.create({
 	viewStyle: {
 		borderWidth: 5,
 		borderColor: 'green',
-		// flexDirection: 'row',
-		// alignItems: 'center',
-		justifyContent: 'space-between',
-		height: 200
+		height: 200,
+		alignItems: 'center'
+		// flexDirection: 'row'
 	},
-	textStyle: {
+	textOneStyle: {
 		borderWidth: 3,
-		borderColor: 'pink'
+		borderColor: 'pink',
+		// flex: 4
+	},
+	textTwoStyle: {
+		borderWidth: 3,
+		borderColor: 'pink',
+		alignSelf: 'stretch'
+		// flex: 4
+	},
+	textThreeStyle: {
+		borderWidth: 3,
+		borderColor: 'pink',
+		// flex: 2
 	}
+	// Save for reference
+	// viewStyle: {
+	// 	borderWidth: 5,
+	// 	borderColor: 'green',
+	// 	flexDirection: 'row',
+	// 	alignItems: 'center',
+	// 	justifyContent: 'space-between',
+	// 	height: 200
+	// },
 })
 
 export default BoxScreen
