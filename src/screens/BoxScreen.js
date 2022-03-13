9fb698bbc4b6a56edd0a022c2c9a8d4fb41c2299
Audry Wolters <audrywolters@gmail.com>
2022-03-13 10:18:56 -0500

    added styles to the View and Text

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,14 +3,23 @@ import { Text, View, StyleSheet } from 'react-native'
 
 const BoxScreen = () => {
 	return (
-		<View>
-			<Text>BoxScreen</Text>
+		<View style={styles.viewStyle}>
+			<Text style={styles.textStyle}>BoxScreen</Text>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	
+	viewStyle: {
+		borderWidth: 3,
+		borderColor: 'black'
+	},
+	textStyle: {
+		borderWidth: 10,
+		borderColor: 'pink',
+		margin: 20,
+		padding: 2
+	}
 })
 
 export default BoxScreen
