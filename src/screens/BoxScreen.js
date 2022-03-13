82400a992bf04a9791110df5d4d423b1270da197
Audry Wolters <audrywolters@gmail.com>
2022-03-13 12:52:36 -0500

    learned a shortcut for RN for fill space. used Absolute. Feels bad practice for me but I guess not as RN made a shortcut for it. There has got to be a web way.

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -13,29 +13,38 @@ const BoxScreen = () => {
 
 const styles = StyleSheet.create({
 	viewStyle: {
-		borderWidth: 5,
-		borderColor: 'green',
+		// borderWidth: 5,
+		// borderColor: 'green',
 		height: 200,
-		alignItems: 'stretch'
+		alignItems: 'stretch',
+		backgroundColor: 'green'
 		// flexDirection: 'row'
+
 	},
 	textOneStyle: {
-		borderWidth: 3,
-		backgroundColor: 'blue',
-		borderColor: 'pink',
+		// borderWidth: 3,
+		backgroundColor: 'lightblue',
+		// borderColor: 'pink',
+		backgroundColor: 'grey'
+
 		// flex: 4
 	},
 	textTwoStyle: {
-		borderWidth: 3,
-		borderColor: 'pink',
-		backgroundColor: 'grey',
+		// borderWidth: 3,
+		// borderColor: 'pink',
+		backgroundColor: 'pink',
 		fontSize: 18,
-		left: 10
+		// position: 'absolute',
+		// top: 0,
+		// right: 0,
+		// bottom: 0,
+		// left: 0
+		...StyleSheet.absoluteFillObject // nice! wish this was in basic css w/o tons of code
 		// flex: 4
 	},
 	textThreeStyle: {
-		borderWidth: 3,
-		borderColor: 'pink',
+		// borderWidth: 3,
+		// borderColor: 'pink',
 		backgroundColor: 'blue',
 		// flex: 2
 	}
